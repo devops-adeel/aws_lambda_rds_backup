46a4c552d6d99265d10889cf43c02dcb45da8f79
Adeel Ahmad <adeelahmad786@gmail.com>
2018-01-03 16:19:24 +0000

    basic restore from latest snapshot

--- lambda_function.py
+++ lambda_function.py
@@ -10,16 +10,68 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 __version__ = "0.1"
 
-import unittest
 import datetime
 import os
-import cfnresponse
+from botocore.exceptions import ClientError
 import boto3
+try:
+    import json
+except ImportError:
+    import simplejson as json
+try:
+    from urllib2 import HTTPError, build_opener, HTTPHandler, Request
+except ImportError:
+    from urllib.error import HTTPError
+    from urllib.request import build_opener, HTTPHandler, Request
 
+SUCCESS = "SUCCESS"
+FAILED = "FAILED"
 RDS = boto3.client('rds')
 DBSNAPSHOTID = os.environ.get('DBSnapshotIdentifier')
 DBINSTANCEID = os.environ.get('DBInstanceIdentifier')
 NOW = datetime.datetime.now()
+
+def send(event, context, response_status, reason= \
+        None, response_data=None, physical_resource_id=None):
+    """
+    building own response function
+    """
+    response_data = response_data or {}
+    response_body = json.dumps(
+        {
+            'Status': response_status,
+            'Reason': reason or "See the details in \
+            CloudWatch Log Stream: " + context.log_stream_name,
+            'PhysicalResourceId': physical_resource_id or context.log_stream_name,
+            'StackId': event['StackId'],
+            'RequestId': event['RequestId'],
+            'LogicalResourceId': event['LogicalResourceId'],
+            'Data': response_data
+        }
+    )
+
+    opener = build_opener(HTTPHandler)
+    request = Request(event['ResponseURL'], data=response_body)
+    request.add_header('Content-Type', '')
+    request.add_header('Content-Length', len(response_body))
+    request.get_method = lambda: 'PUT'
+    try:
+        response = opener.open(request)
+        print("Status code: {}".format(response.getcode()))
+        print("Status message: {}".format(response.msg))
+        return True
+    except HTTPError as exc:
+        print("Failed executing HTTP request: {}".format(exc.code))
+        return False
+
+def get_my_log_stream(context):
+    """
+    Logging function for the lambda handler to call.
+    """
+    print("Log stream name:", context.log_stream_name + '\n' + "Log group name:", \
+            context.log_group_name + '\n' +  "Request ID:", context.aws_request_id \
+            + '\n' +  "Mem. limits(MB):", context.memory_limit_in_mb + '\n' + \
+            "Time remaining (MS):", context.get_remaining_time_in_millis())
 
 def handler(event, context):
     """
@@ -34,21 +86,12 @@ def handler(event, context):
             )
         response['DBSnapshot'].pop('SnapshotCreateTime')
         response['DBSnapshot'].pop('InstanceCreateTime')
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, response)
+        send(event, context, SUCCESS, response)
 
-    except Exception as error:
+    except ClientError as error:
         print(error)
 
 if __name__ == "__main__":
     handler(event, context)
     import doctest
     doctest.testmod()
-    class MyTest(unittest.TestCase):
-        """
-        Class to initiate to test function
-        """
-        def test(self):
-            """
-            Test Function
-            """
-            self.assertEqual(handler(event, context),)
--- rds_restore.py
+++ rds_restore.py
@@ -6,60 +6,54 @@ Author:         Adeel Ahmad
 Description:    Python Script to restore from RDS Backup
 """
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, \
+        division, print_function, unicode_literals
 
 __version__ = "0.1"
 
-import unittest
-from datetime import datetime
+import os
 from botocore.exceptions import ClientError
 import boto3
 
 RDS = boto3.client('rds')
-DBINSTANCEID = 'pass'
+INSTANCEID = os.environ.get('DBINSTANCEID')
+
 
 def retrieve_latest_snapshot(instanceid):
     """
     Function to retrieve latest snapshot
     """
-
     try:
         snapshots = RDS.describe_db_snapshots(
             DBInstanceIdentifier=instanceid
             )
-        newest = max(snapshots['DBSnapshots'].itervalues('SnapshotCreateTime'),
-                     key=lambda latest: latest if isinstance(latest, datetime) else datetime.min)
-
+        newest = sorted(snapshots['DBSnapshots'],
+                        key=lambda latest: latest['SnapshotCreateTime'],
+                        reverse=True)[0]['DBSnapshotIdentifier']
         return newest
 
     except ClientError as error:
         print(error)
+
 
 def main(instanceid):
     """
     Main function restore from latest snapshot.
     """
     try:
-        newest = retrieve_latest_snapshot(instanceid)
+        snapshot_id = retrieve_latest_snapshot(instanceid)
 
         restore = RDS.restore_db_instance_from_db_snapshot(
             DBInstanceIdentifier=instanceid,
-            DBSnapshotIdentifier=newest
+            DBSnapshotIdentifier=snapshot_id
             )
-        return restore['DBInstance']['StatusInfos']['Status']
+        # return restore['DBInstance']['StatusInfos']['Status']
+        print(restore['DBInstance']['StatusInfos']['Status'])
     except ClientError as error:
         print(error)
 
+
 if __name__ == "__main__":
-    main(instanceid)
+    main(INSTANCEID)
     import doctest
     doctest.testmod()
-    class MyTest(unittest.TestCase):
-        """
-        Class to initiate to test function
-        """
-        def test(self):
-            """
-            Test Function
-            """
-            self.assertEqual(main(instancid))
