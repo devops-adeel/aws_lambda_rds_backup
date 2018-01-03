b4e156223d149a3424fee2613b84122ffb8cc264
Adeel Ahmad <adeelahmad786@gmail.com>
2018-01-03 18:42:37 +0000

    used the boto library for restoring latest snapshot

--- rds_restore.py
+++ rds_restore.py
@@ -19,23 +19,6 @@ RDS = boto3.client('rds')
 INSTANCEID = os.environ.get('DBINSTANCEID')
 
 
-def retrieve_latest_snapshot(instanceid):
-    """
-    Function to retrieve latest snapshot
-    """
-    try:
-        snapshots = RDS.describe_db_snapshots(
-            DBInstanceIdentifier=instanceid
-            )
-        newest = sorted(snapshots['DBSnapshots'],
-                        key=lambda latest: latest['SnapshotCreateTime'],
-                        reverse=True)[0]['DBSnapshotIdentifier']
-        return newest
-
-    except ClientError as error:
-        print(error)
-
-
 def query_db_cluster(instanceid):
     """
     Querying whether DB is Clustered or not
@@ -45,9 +28,7 @@ def query_db_cluster(instanceid):
             DBInstanceIdentifier=instanceid
             )
         return db_instance['DBInstances'][0]['DBClusterIdentifier']
-
     except KeyError:
-        # print("Not part of a DB Cluster")
         return False
 
 
@@ -55,17 +36,29 @@ def main(instanceid):
     """
     Main function restore from latest snapshot.
     """
-    try:
-        snapshot_id = retrieve_latest_snapshot(instanceid)
-
-        restore = RDS.restore_db_instance_from_db_snapshot(
-            DBInstanceIdentifier=instanceid,
-            DBSnapshotIdentifier=snapshot_id
-            )
-        # return restore['DBInstance']['StatusInfos']['Status']
-        print(restore['DBInstance']['StatusInfos']['Status'])
-    except ClientError as error:
-        print(error)
+    if query_db_cluster(instanceid):
+        cluster_id = query_db_cluster(instanceid)
+        new_cluster_id = os.environ.get('NEW_CLUSTER_ID')
+        try:
+            restore = RDS.restore_db_cluster_to_point_in_time(
+                DBClusterIdentifier=new_cluster_id,
+                SourceDBClusterIdentifier=cluster_id,
+                UseLatestRestorableTime=True
+                )
+            return restore['DBCluster'][0]['Status']
+        except ClientError as error:
+            print(error)
+    else:
+        new_instanceid = os.environ.get('NEW_INSTANCEID')
+        try:
+            restore = RDS.restore_db_instance_to_point_in_time(
+                SourceDBInstanceIdentifier=instanceid,
+                TargetDBInstanceIdentifier=new_instanceid,
+                UseLatestRestorableTime=True
+                )
+            return restore['DBInstance'][0]['DBInstanceStatus']
+        except ClientError as error:
+            print(error)
 
 
 if __name__ == "__main__":
